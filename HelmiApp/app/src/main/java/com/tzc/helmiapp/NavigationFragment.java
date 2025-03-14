8569550619b906868bf4894116335db437efe47e
fenglan12 <2867423475@qq.com>
2025-03-14 19:26:57 +0800

    hmx-setting.xml-finish

--- HelmiApp/app/src/main/java/com/tzc/helmiapp/NavigationFragment.java
+++ HelmiApp/app/src/main/java/com/tzc/helmiapp/NavigationFragment.java
@@ -1,18 +1,11 @@
 package com.tzc.helmiapp;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link NavigationFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class NavigationFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
@@ -53,12 +46,5 @@ public class NavigationFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_navigation, container, false);
     }
 }
